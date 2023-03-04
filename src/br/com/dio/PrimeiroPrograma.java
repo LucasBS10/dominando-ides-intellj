57257be4902b74840306642301447346e7424c47
LucasBS10 <lucasbernardo2604@gmail.com>
2023-03-04 20:13:24 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.Model.Gato;
+
+import java.util.Scanner;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        //Scanner scan = new Scanner(System.in);
+        Gato gato = new Gato();
+        System.out.println(gato);
+        //scan.nextInt();
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World" + (a + b));*/
+    }
+}
